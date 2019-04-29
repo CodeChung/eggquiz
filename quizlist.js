35db372d9612a6be023671556082ae606989d0b1
Harry Chung <hc9825@gmail.com>
2019-04-29 14:55:56 -0700

    added quiz object and modified css

---
+++ quizlist.js
@@ -0,0 +1,52 @@
+const quizList = [
+    {
+        question: "What’s the nutritional difference between a brown egg and a white egg?",
+        choices: ["Brown eggs have more Vitamin A", "White eggs have more protein", "Brown eggs are dirtier", "There is no difference"],
+        answer: "There is no difference"
+    },
+    {
+        question: "What determines whether hens lay brown or white eggs?",
+        choices: ["The hen’s age", "The hen’s diet", "The hen’s temperature", "The hen’s ear color"],
+        answer: "The hen’s ear color"
+    },
+    {
+        question: "Which country produces the most eggs?",
+        choices: ["USA", "Japan", "China", "India"],
+        answer: "China"
+    },
+    {
+        question: "How many eggs does a hen lay in a year?",
+        choices: ["260", "420", "650", "1,000"],
+        answer: "260"
+    },
+    {
+        question: "How long does it take to soft boil an egg?",
+        choices: ["1 minute", "3 minutes", "5 minutes", "7 minutes"],
+        answer: "5 minutes"
+    },
+    {
+        question: "How can you tell if an egg has gone bad?",
+        choices: ["Bad eggs float in water", "Bad eggs turn blue in vinegar", "Bad eggs whisper bad things when you put then next to your ear", "Bad eggs smell like wine"],
+        answer: "5 minutes"
+    },
+    {
+        question: "What is the record for most omelets made in 30 minutes?",
+        choices: ["35", "199", "315", "427"],
+        answer: "427"
+    },
+    {
+        question: "What happens as a hen gets older?",
+        choices: ["Egg becomes smaller", "Egg becomes larger", "Egg becomes darker", "Egg becomes thicker"],
+        answer: "Egg becomes larger"
+    },
+    {
+        question: "Which egg contains the least fat?",
+        choices: ["Goose", "Quail", "Chicken", "Duck"],
+        answer: "Quail"
+    },
+    {
+        question: "How long does a hard boiled egg last in a fridge?",
+        choices: ["One week", "One fortnight", "One month", "Two months"],
+        answer: "One week"
+    },
+]
